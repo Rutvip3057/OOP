7399b7737ee9221b725064eaaac137b4186e12f1
Rutvi Patel <patelrutvi3057@gmail.com>
2026-04-27 01:10:39 +0530

    Add files via upload

---
+++ practical 19.java
@@ -0,0 +1,44 @@
+class Student {
+    protected int rollNo;
+    protected String name;
+
+    public Student(int rollNo, String name) {
+        this.rollNo = rollNo;
+        this.name = name;
+    }
+
+    public void displayStudent() {
+        System.out.println("Roll No: " + rollNo);
+        System.out.println("Name: " + name);
+    }
+}
+
+class Result extends Student {
+    private int marks1, marks2, marks3;
+
+    public Result(int rollNo, String name, int m1, int m2, int m3) {
+        super(rollNo, name);
+        this.marks1 = m1;
+        this.marks2 = m2;
+        this.marks3 = m3;
+    }
+
+    public void displayResult() {
+        displayStudent();
+        int total = marks1 + marks2 + marks3;
+        double average = total / 3.0;
+
+        System.out.println("Marks1: " + marks1);
+        System.out.println("Marks2: " + marks2);
+        System.out.println("Marks3: " + marks3);
+        System.out.println("Total: " + total);
+        System.out.println("Average: " + average);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Result r = new Result(101, "John", 75, 80, 85);
+        r.displayResult();
+    }
+}
